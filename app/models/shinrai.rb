d89b4cc2e5e420918b4da669140ea2920ae55698
西小倉宏信 <nishiko@mindia.jp>
2018-12-16 03:32:53 +0000

    add Shinrai

---
+++ app/models/shinrai.rb
@@ -0,0 +1,9 @@
+class Shinrai
+  def campfire
+    '相手を信頼して任せよう。信頼とは、疑うことではなく、任せるところから始まる。信頼するとは、見返りを求めず、相手の失敗をも引き受けるという態度である。'
+  end
+
+  def campfire_url
+    'https://campfire.co.jp/vision/'
+  end
+end
